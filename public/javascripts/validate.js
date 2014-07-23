172390b1b48e6b01466f8dd8161c69a9af67bd84
sadaka <rakesh_sadaka@yahoo.com>
2014-07-23 14:54:50 +0530

    now validate.js can handle multiple forms in single webpage

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -1,5 +1,5 @@
-var validate = function (formId, rules){
-  var regex = {
+var validate = {
+  regex:{
     email: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
     varchar:/^[a-zA-Z\s]+$/,
     general:/^[0-9a-zA-Z\s_\-'\.\&\!\@]+$/,
@@ -8,131 +8,95 @@ var validate = function (formId, rules){
     integer:/^\d+$/,
     youtubeUrl:/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/,
     url:/^((https?|ftp):\/\/|(www|ftp)\.)[a-z0-9-]+(\.[a-z0-9-]+)+([\/?].*)?$/i
-  };
-  
-  $('#'+formId).submit(function(e){
-    tt();
+  },
+  register:function(formId, rules){
+    this.rules = rules;
+    this.formId = formId;
+    $('#'+formId).submit(this.init.bind(this));
+  },
+  init:function(e){
+    var rules = this.rules;
     $('span.error').remove();
     var error = false;
-    $('#'+formId +' input,select,textarea').filter(':input').each(function(){
-      if(rules[$(this).attr('name')]){
-        var val = $(this).val().trim();
-        if($.type(rules[$(this).attr('name')]) == 'object'){
-          for(j in rules[$(this).attr('name')]){
-            switch (j){
-              case 'required':
-                if(val == ''){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')][j] || $(this).attr('name') + ' is required.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-              case 'email':
-                if(!val.match(regex.email)){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')][j] || 'Please enter valid email.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-              case 'integer':
-                if(!val.match(regex.integer)){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')] || 'Please enter integer value.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-              case 'numeric':
-                if(!val.match(regex.numeric)){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')] || 'Please enter numeric value.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-              case 'char':
-              case 'varchar':
-                if(!val.match(regex.varchar)){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')] || 'Please enter valid value.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-              case 'general':
-              default:
-                if(!val.match(regex.general)){
-                  error = true;
-                  var msg = rules.messages[$(this).attr('name')] || 'Please enter valid '+$(this).attr('name')+'.' ;
-                  $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-                }
-              break;
-            }
+    
+    $($('#'+this.formId)).find('input,select,textarea').filter(':input').each(function(i, el){
+      var elName = $(el).attr('name');
+      if(rules[elName]){
+        var val = $(el).val().trim();
+        
+        if($.type(rules[elName]) == 'object'){
+          for(j in rules[elName]){
+            if(!this.validate(j, el, val))
+              error = true;
           }
         }else{
-          switch (rules[$(this).attr('name')]){
-            case 'required':
-              if(val == ''){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || $(this).attr('name') + ' is required.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-            case 'email':
-              if(!val.match(regex.email)){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || 'Please enter valid email.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-            case 'integer':
-              if(!val.match(regex.integer)){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || 'Please enter integer value.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-            case 'numeric':
-              if(!val.match(regex.numeric)){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || 'Please enter numeric value.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-            case 'char':
-            case 'varchar':
-              if(!val.match(regex.varchar)){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || 'Please enter valid value.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-            case 'general':
-            default:
-              if(!val.match(regex.general)){
-                error = true;
-                var msg = rules.messages[$(this).attr('name')] || 'Please enter valid '+$(this).attr('name')+'.' ;
-                $("<span class='error'>"+msg+"</span>").insertAfter($(this));
-              }
-            break;
-          }
-        }
-        
+          if(!this.validate(rules[elName], el, val))
+            error = true;
+        }        
       }
-    });
-    if(error)
-      e.preventDefault();
+    }.bind(this));
+    if(error){
+      e.preventDefault();      
+      return false;
+    }
+    return;
+  },
+  validate:function(validation, el, val){
+    var elName = $(el).attr('name');
+    var rules = this.rules;
+    if(rules.messages[elName]){
+      if($.type(rules[elName]) == 'object')
+        var m = rules.messages[elName][validation];
+      else
+        var m = rules.messages[elName];
+    }
+    
+    switch (validation){
+      case 'required':
+        if(val == ''){
+          var msg = m|| elName + ' is required.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+      case 'email':
+        if(!val.match(this.regex.email)){
+          var msg = m || 'Please enter valid email.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+      case 'integer':
+        if(!val.match(this.regex.integer)){
+          var msg = m || 'Please enter integer value.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+      case 'numeric':
+        if(!val.match(this.regex.numeric)){
+          var msg = m || 'Please enter numeric value.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+      case 'char':
+      case 'varchar':
+        if(!val.match(this.regex.varchar)){
+          var msg = m || 'Please enter valid value.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+      case 'general':
+      default:
+        if(!val.match(this.regex.general)){
+          var msg = m || 'Please enter valid '+elName+'.' ;
+          $("<span class='error'>"+msg+"</span>").insertAfter($(el));
+          return false;
+        }
+      break;
+    }
     return true;
-  });
-  return;
-}
-
-
-
-validate.prototype.validateYoutubeUrl = function (url){
-    var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-    // return (url.match(p)) ? RegExp.$1 : false;
-    return (url.match(p)) ? true : false;
-}
-
-validate.prototype.validateUrl = function (url){
-    var reg = /^((https?|ftp):\/\/|(www|ftp)\.)[a-z0-9-]+(\.[a-z0-9-]+)+([\/?].*)?$/i;    
-    return url.match(reg)?true:false;
-}
+  }
+};
